c68e73ec317e3a5800d9b300c7882f68ada00d96
10725114655 <10725114655@UNS07DT99843.animaedu.intranet>
2025-05-12 09:55:20 -0300

    Exemplo JFrame

---
+++ src/main/java/principal/ExemploFrmTriangulo.java
@@ -0,0 +1,10 @@
+package principal;
+import view.FrmTriangulo;
+public class ExemploFrmTriangulo {
+
+    public static void main(String[] args) {
+       FrmTriangulo janela = new FrmTriangulo();
+       janela.setVisible(true);
+       
+    }
+}
